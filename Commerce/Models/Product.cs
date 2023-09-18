753e7f841d86c3bed7dfd67ab1fe3ce4021b2c58
vvicdevv <victoriafarina.dev@gmail.com>
2023-09-18 15:31:45 -0300

    image set up

--- Commerce/Models/Product.cs
+++ Commerce/Models/Product.cs
@@ -11,14 +11,25 @@ namespace Commerce.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class Product
     {
         public int ProductId { get; set; }
+
+        [Display(Name ="Name")]
         public string ProductName { get; set; }
+
+        [Display(Name ="Size")]
         public string ProductSize { get; set; }
+
+        [Display(Name ="Quantity")]
         public Nullable<int> ProductQty { get; set; }
+
+        [Display(Name ="Brand")]
         public string ProductBrand { get; set; }
+        [Display(Name ="Image")]
         public string ProductPic { get; set; }
+        
     }
 }
